bc47d0552fba600f2081a9b73cd9e6bd333b0c29
Stephen Sheldon <20730278+ssheld@users.noreply.github.com>
2021-11-27 09:54:41 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+<h1> STM32 Library for US-100 Ultrasonic Distance Sensor </h1>
+
+STM32 library for using the <a href="https://www.adafruit.com/product/4019">US-100 Ultrasonic Distance Sensor</a>. The tutorial in this repository utilizes the [SensorTile development kit](https://www.st.com/en/evaluation-tools/steval-stlkt01v1.html).
+
+
+***Repository Contents***
+---
+- **/us100_example** - Example program utilizing US-100 library.
+- **/US100** - US-100 library.
+- **/datasheets** - Datasheets for all hardware utilizes in the example program.
+- **us100 tutorial.pdf/.docx** - Tutorial on how to setup hardware and library.
+
+***Caveats***
+---
+This library currently does not implement the temperature sensing functionality of the US-100 Ultrasonic Distance Sensor.
---
+++ us100_example/Debug/US100/subdir.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../US100/us100.c 
+
+OBJS += \
+./US100/us100.o 
+
+C_DEPS += \
+./US100/us100.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+US100/%.o: ../US100/%.c US100/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32L476xx -c -I../Core/Inc -I../Drivers/STM32L4xx_HAL_Driver/Inc -I../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32L4xx/Include -I../Drivers/CMSIS/Include -I"/Users/stephensheldon/STM32CubeIDE/workspace_1.7.0/stm32-us100-library/us100_example/US100" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
